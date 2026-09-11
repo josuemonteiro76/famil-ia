1baf75c49eaa4d8bb723f2eda3b185aa576bc21e
Josué da Silva Monteiro <135916396+josuemonteiro76@users.noreply.github.com>
2026-09-10 21:53:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const app = initializeApp(firebaseConfig);
+import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc, onSnapshot, deleteDoc, doc, updateDoc, query, orderBy, serverTimestamp } 
 from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
 
